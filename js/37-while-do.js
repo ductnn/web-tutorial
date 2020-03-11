f969ed6420a9e24ed576c5043ac364c5cb838141
DuctnNetwork <trannhuduc531998@gmail.com>
2020-03-11 18:54:35 +0700

    Add 37 38

--- js/37-while-do.js
+++ js/37-while-do.js
@@ -14,3 +14,13 @@ function sum(x) {
 }; 
 
 console.log(sum(3));
+//////////////////////////////////////////////////////
+
+/**
+ * Sử dụng vòng lặp while để tìm bội số chung lớn nhất của 3 và 5 trong khoảng từ 0 đến 1000
+ */
+ var count = 1000;
+ while(count--){
+ 	if(count % 3 == 0 && count % 5 == 0)
+ 		return count;
+ };
