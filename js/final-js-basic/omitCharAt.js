df2f4013f5b4bd02398a11b33226ea024542f254
DuctnNetwork <trannhuduc531998@gmail.com>
2020-03-14 12:32:03 +0700

    Add omitCharAt

---
+++ js/final-js-basic/omitCharAt.js
@@ -0,0 +1,15 @@
+//Viết function omitCharAt  trả về chuỗi đã được loại bỏ ký tại vị trí n bất kỳ
+//Lưu ý cho chuỗi 'abcd' :
+// 		element    'a'|'b'|'c'|'d'
+//		index 	    0	|	1	|	2	|	3
+//  n = 1 ------------------- 'acd'
+//  n = 2 ------------------- 'abd'
+
+function omitCharAt(str, n) {
+  // viết code ở đây.	
+  var arr = str.split("");
+  arr.splice(n, 1);
+  return arr.join("");
+};
+
+console.log(omitCharAt("abcd", 2));
